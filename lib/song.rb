c3a62b5435efb3be54d432e9bc1ce0ae47b43a95
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-19 00:08:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,9 +13,9 @@ class Song
     @@all << self
   end
 
-  def self.find_by_name(name)
-    self.all.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   self.all.detect{|a| a.name == name}
+  # end
 
   def artist=(artist)
     @artist = artist
