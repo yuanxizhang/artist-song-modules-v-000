16908d1c90127d3952f73d9497d8e4ed291b7532
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-19 00:14:26 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -40,8 +40,8 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 
 end
